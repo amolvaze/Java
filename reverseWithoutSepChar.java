a48d77d8ee249128ec5dded5275f9bc17572d8a8
Amol Vaze <mailmeamol2112@gmail.com>
2020-07-02 18:55:43 -0500

    Create reverseWithoutSepChar.java

---
+++ reverseWithoutSepChar.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+// code to reverse a given string for all alphabets without special characters
+
+class Main {
+ 
+  public static void reverseString(char str[]){
+    
+    // left and right pointers are initalized.
+    int left= 0, right = str.length-1;
+    // loop through string
+   
+    while(left < right) {
+      
+      // ignore special characters.
+    if(!Character.isAlphabetic(str[left])){ left++; }
+    else if(!Character.isAlphabetic(str[right])) { right--;}
+     
+    // if there is alphabets
+
+   else {
+       char temp = str[left];
+       str[left] = str[right];
+       str[right] = temp;
+       left++;
+       right--;
+
+   }
+      
+    }
+
+
+  }
+
+  public static void main(String[] args) {
+   // String str = "a!!!b.c.d,e'f,ghi";
+    String str = "+h&e/! L@_L! O? ";
+    char[] charArray = str.toCharArray();
+   System.out.println("Input String: "+str);
+   reverseString(charArray);
+   String revStr= new String(charArray);
+   System.out.println("Output String: "+revStr);
+
+  }
+}
