5bb6d9d65421b33ead50c2d411b37f0a6500adf0
Amol Vaze <mailmeamol2112@gmail.com>
2020-07-05 17:22:41 -0500

    Update interSectionArrays.java

--- interSectionArrays.java
+++ interSectionArrays.java
@@ -49,3 +49,55 @@ public static void listToArray(List<Integer> list) {
     // }
   }
 }
+
+/// -------------------------------------------------------------------------Using same above code with return type integer[]--------------------------------------------------
+
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if(nums1.length == 0 || nums2.length == 0 || nums1 == null || nums2 == null) {
+            //System.out.println("Empty input");
+            return new int[0];
+        }
+        
+        // put first array contents to map.
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num: nums1){
+         if(map.containsKey(num)){
+             map.put(num, map.get(num) + 1);
+         }
+            else{
+                map.put(num, 1);
+            }
+        }
+        
+      // Loop thru the list for second array contents to compare
+        List<Integer> list = new ArrayList<>();
+        for(int num: nums2){
+          if(map.containsKey(num) && map.get(num) > 0){
+              list.add(num);
+              int freq = map.get(num);
+              freq--;
+              map.put(num, freq);
+          }
+        }
+        
+      return listToArray(list);  
+        
+    }
+    
+    public static int[] listToArray(List<Integer> list) {
+      int[] resultArray = new int[list.size()];
+      for(int i=0; i< list.size(); i++) {
+          resultArray[i] = list.get(i);
+      }
+      
+        // for(int i= 0; i< resultArray.length; i++){
+        //     System.out.println(resultArray[i]+" ");
+        // }
+        return resultArray;
+    }
+    
+    
+}
