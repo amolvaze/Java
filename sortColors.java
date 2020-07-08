d916a2cb39e91ec24a44e2a402e6ce46f7a58779
Amol Vaze <mailmeamol2112@gmail.com>
2020-07-07 22:33:55 -0500

    Update sortColors.java

--- sortColors.java
+++ sortColors.java
@@ -43,3 +43,59 @@ class Main {
     
   }
 }
+// ----------------------------------------------------------Using Extra Temp variable to swap array values --------------------------------------------------------------
+
+
+public static void sortColors(int[] arr) {
+        int lo= 0;
+        int mid=0;
+        int hi= arr.length-1;
+        while(mid<=hi){
+           int val = arr[mid];
+            if(val == 0){
+                int temp= arr[lo];
+                arr[lo] = arr[mid];
+                arr[mid] = temp;
+                lo++;
+                mid++;
+            }
+            else if(val ==1){
+                mid++;
+            }
+            else{
+                int temp = arr[hi];
+                arr[hi] = arr[mid];
+                arr[mid] = temp;
+                hi--;
+                
+            }
+            
+            
+            
+        }
+        
+   for(int i=0; i< arr.length; i++) {
+      System.out.println(arr[i]+" ");
+    }
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
