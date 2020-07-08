35771e33ea28939b91d3269d467b835417a6ac55
Amol Vaze <mailmeamol2112@gmail.com>
2020-07-08 18:47:13 -0500

    Update longestCommonPrefix.java

--- longestCommonPrefix.java
+++ longestCommonPrefix.java
@@ -7,7 +7,7 @@ class Main {
          return " ";
       }
        String prefix = strs[0];
-       for(int i=0 ; i< strs.length; i++) {
+       for(int i=1 ; i< strs.length; i++) {
          while(strs[i].indexOf(prefix) != 0){
           prefix = prefix.substring(0, prefix.length()-1);
          }
