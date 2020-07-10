c5892c6193ea954c106b983c84bbd6b44e0be7f7
Amol Vaze <mailmeamol2112@gmail.com>
2020-07-10 18:36:41 -0500

    Update DeleteLinkedListNode.java

--- DeleteLinkedListNode.java
+++ DeleteLinkedListNode.java
@@ -69,3 +69,20 @@ printList(head);
     //printList(head);
 	}
 }
+//---------------------- Leetcode solution----------------------------
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next= node.next.next;
+    }
+}
+
+
