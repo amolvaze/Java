2fa4c97fc9ef512ccf102a11e7ea825ec8b63a59
Amol Vaze <mailmeamol2112@gmail.com>
2020-07-14 18:50:32 -0500

    Create middileOfTheLinkedList.java

---
+++ middileOfTheLinkedList.java
@@ -0,0 +1,28 @@
+// code to return the middle of the singly linked list.
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    public ListNode middleNode(ListNode head) {
+       // Using 2 pointers approach slow and fast. 
+       //Advance fast as two steps ahead of that slow pointer.
+       ListNode slow = head;
+       ListNode fast = head;
+      
+       while(fast!=null && fast.next != null) 
+       {
+            slow = slow.next;
+            fast= fast.next.next;
+       }
+        
+       return slow; 
+    }
+};
