79899acbfce342c60c635f654889f8b3b4a0f5fc
Amol Vaze <mailmeamol2112@gmail.com>
2020-07-20 21:49:06 -0500

    Create twoSum.java

---
+++ twoSum.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+// code for Two Sum using a hashmap in lienar time O(n) and space O(n)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        // loop thru given input array
+        for(int i=0; i< nums.length; i++) {
+            int diff = target - nums[i];
+            if(map.containsKey(diff)) {
+              result[0] = map.get(diff);
+              result[1] = i;
+            }
+            else {
+               map.put(nums[i], i);
+            }
+        }
+      
+        return result;
+        
+    }
+}
