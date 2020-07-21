d07a314a35913b46d4aa1574f151c4133703871d
Amol Vaze <mailmeamol2112@gmail.com>
2020-07-21 18:37:42 -0500

    Create subsets.java

---
+++ subsets.java
@@ -0,0 +1,25 @@
+import java.util.*;
+// code to return subsets of nos
+class Solution {
+    List<List<Integer>> result = new ArrayList();
+    public List<List<Integer>> subsets(int[] nums) {
+        if(nums.length == 0) 
+            return result;
+        backTrack(0, new ArrayList<Integer>(), nums);
+        return result;
+    }
+    
+    public void backTrack(int start, ArrayList<Integer> curr , int[] nums) {
+        result.add(new ArrayList<Integer> (curr));
+        
+        // loop thru 
+        for(int i =start; i< nums.length; i++) {
+            curr.add(nums[i]);
+            backTrack(i+1, curr, nums);
+            curr.remove(curr.size()- 1);
+        }
+        
+        
+    }
+    
+}
