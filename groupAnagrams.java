0daf953cbed3ef1bc46a3eac0aaf0fc11de3ec9a
Amol Vaze <mailmeamol2112@gmail.com>
2020-08-01 15:50:38 -0500

    Update groupAnagrams.java

--- groupAnagrams.java
+++ groupAnagrams.java
@@ -1,37 +1,64 @@
 import java.util.*;
-// code to return group anagrams.
+//code to return group anagrams
 // Both time and space complity = O(nk) where n is the length of strs and k is the max length of strs in a string.
 //  Counting each string is linear in the size of the string, and we count every string.
+import java.util.*;
+
+// code to group anagrams - 0(nk) time and space complexity.
+
 class Main {
+
+  public static List<List<String>> groupAnagrams(String[] strs) { 
+ List<List<String>> res = new ArrayList<>();
+   if(strs.length == 0)
+   return res;
+ 
+ HashMap<String, ArrayList<String>> map  = new HashMap<>();
     
-    public static List<List<String>> groupAnagrams(String[] strs) {
-    if(strs.length == 0) { return new ArrayList(); }
-   // List<List<String>> al = new ArrayList<>();
-    HashMap<String, List<String>> map = new HashMap<>();  
-    int[] count = new int[26];
-    for(String s: strs) {
-     Arrays.fill(count, 0);
-     for (char c : s.toCharArray()) count[c - 'a']++; 
-     StringBuilder sb = new StringBuilder();
-     for(int i=0 ; i< 26; i++) {
-       sb.append('#');
-       sb.append(count[i]);     
-       }
-    
-    String key = sb.toString();
-    if(!map.containsKey(key))
-      map.put(key, new ArrayList());
-      map.get(key).add(s);
+   for(String str : strs)
+   {
+      int[] count = new int[26];
+      for(int i=0; i < str.length(); i++)
+      {
+        count[str.charAt(i) - 'a']++;
+      }
+
+   
+
+   StringBuilder sb = new StringBuilder();
+   for(int c : count)
+    {
+      sb.append(c);
+      sb.append('#');
     }
-   // return al;
-   return new ArrayList(map.values());
-   }
-
-
-    public static void main(String[] args) {
-   // String str = "babad";
-  String[] str = {"eat", "tea", "tan", "ate", "nat", "bat"};
-    System.out.println(groupAnagrams(str));
+  
+  String rep = sb.toString();
+  
+  if(map.containsKey(rep))
+  {
+  ArrayList<String> ana = map.get(rep);
+  ana.add(str);
+  map.put(rep, ana);
   }
-    
+  else
+  {
+  ArrayList<String> ana = new ArrayList<>();
+  ana.add(str);
+  map.put(rep, ana);
+  }
+}
+
+for(List<String> val : map.values())
+ {
+   res.add(val);
+ }
+
+return res;
+
+  }
+  public static void main(String[] args) {
+
+   String[] arr = { "eat", "tea", "tan", "ate", "nat", "bat"}; 	
+    System.out.println(groupAnagrams(arr)); 
+  }
 }
