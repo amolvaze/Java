d6b5a830b696a2fffab4ebf808a72fd5d5f3f9b8
Amol Vaze <mailmeamol2112@gmail.com>
2020-08-04 18:52:22 -0500

    Update rangeSumQuery.java

--- rangeSumQuery.java
+++ rangeSumQuery.java
@@ -4,6 +4,15 @@ import java.util.*;
 
 // Time and space complexity - O(n)- dynamic programming approach. 
 
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(i,j);
+ */
+//  0   1  2   3  4   5
+// [-2, 0, 3, -5, 2, -1]
+//  0   -2 -2  1  -4  -2  -3
+
 class Main {
   int[] dp;
   public NumArray(int[] nums) {
@@ -16,7 +25,7 @@ class Main {
   }
  
   public int sumRange(int i, int j) {
-        return dp[j+1]-dp[i];
+        return dp[j+1]-dp[i]; // This sum will take O(1) each time.
     } 
 
   public static void main(String[] args) {
