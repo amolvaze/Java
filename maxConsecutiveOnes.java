2f17bc5861332dbddcd4c48a07cd58e935d8d21d
Amol Vaze <mailmeamol2112@gmail.com>
2020-08-20 15:44:59 -0500

    Create maxConsecutiveOnes.java

---
+++ maxConsecutiveOnes.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+// Code to find the max consecutive ones.
+
+// Time Complexity - O(n)
+// Space Complexity - O(n)
+
+class Main {
+   public static int findMaxConsecutiveOnes(int[] nums) {
+     int max =0;
+     int no_of_ones= 0;
+    for(int i=0; i< nums.length; i++)
+    {
+      if(nums[i] == 1)
+      {
+       no_of_ones += 1;
+       max = Math.max(max, no_of_ones);
+      }
+      else
+      {
+        no_of_ones =0;
+      }
+    }
+
+     return max; 
+   }
+  public static void main(String[] args) {
+    int[] arr = {1,1,0,1,1,1};
+    System.out.println(findMaxConsecutiveOnes(arr));
+  }
+}
