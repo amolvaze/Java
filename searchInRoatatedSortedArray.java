6478f2f9c31bb5f9ef10a397b0e74900b8c4e587
Amol Vaze <mailmeamol2112@gmail.com>
2020-10-08 16:05:20 -0500

    Update searchInRoatatedSortedArray.java

--- searchInRoatatedSortedArray.java
+++ searchInRoatatedSortedArray.java
@@ -45,3 +45,37 @@ class Solution {
         
     }
 }
+
+
+// Another Approach
+
+class Solution {
+    public int search(int[] nums, int target) { 
+        if(nums.length == 0|| nums == null) return -1;
+        int left = 0;
+        int right = nums.length-1;
+        int mid;
+        while(left<=right)
+        {
+            mid = (left+right)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>=nums[left])
+            {
+                if(target<=nums[mid] && target>=nums[left])
+                    right = mid-1;
+                else
+                    left = mid+1;
+            }
+            else
+            {
+                if(target>=nums[mid] && target<=nums[right])
+                    left = mid+1;
+                else
+                    right = mid-1;
+            }
+        }
+        return -1;
+    }
+}
+
