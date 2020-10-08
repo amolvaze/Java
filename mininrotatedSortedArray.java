f804e1805e73a0cd79f1568e3f8202df1cda0971
Amol Vaze <mailmeamol2112@gmail.com>
2020-10-08 15:32:04 -0500

    Update mininrotatedSortedArray.java

--- mininrotatedSortedArray.java
+++ mininrotatedSortedArray.java
@@ -33,3 +33,46 @@ class Main {
     System.out.println(minRotatedSorted(arr));
   }
 }
+
+// To find the min in rotated sorted array when array has the duplicates.
+
+class Solution {
+ // private static int[] removeDuplicates(int[] num) {
+  //   Set<Integer> set = new HashSet();
+  //        for(int i:num){
+  //           set.add(i);
+  //       }
+  //     return num;
+  // }
+ 
+   // Modified above function - now working properly!
+  private static int[] removeDuplicates(int[] nums) {
+    int i = 0;
+    for (int j = 1; j < nums.length; j++) {
+        if (nums[j] != nums[i]) {
+           i++;
+            nums[i] = nums[j];
+        }
+   }
+    return nums;
+}
+  public int findMin(int[] nums) {
+       int[] result = removeDuplicates(nums);
+        if(result.length == 0) return 0;
+        if(result.length == 1) return nums[0];
+        int left = 0;
+        int right = result.length-1;
+        while(left < right) {
+         int mid  = left + (right-left) /2;
+        if(mid > 0 && result[mid] < result[mid-1]){
+            return result[mid];
+        } else if(result[left] <= result[mid] && result[mid] > result[right]){
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+        }
+        
+        return result[left];
+    }
+}
