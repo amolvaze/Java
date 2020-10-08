e80b2ab31d9f3b3b2027be268031c8a23d09c65c
Amol Vaze <mailmeamol2112@gmail.com>
2020-10-08 18:25:04 -0500

    Update Kth-Largest-Element-from-an-Array.java

--- Kth-Largest-Element-from-an-Array.java
+++ Kth-Largest-Element-from-an-Array.java
@@ -26,3 +26,18 @@ public static void main(String[] args) {
 
   }
 }
+
+// Another approach --similar to above
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int ele: nums){
+            pq.add(ele);
+            if(pq.size() > k){
+               pq.poll();
+            }
+        }
+        return pq.poll();
+    }
+}
