758ed205bd6941227ab920541eff1da8e7d5744b
Amol Vaze <mailmeamol2112@gmail.com>
2020-10-09 11:23:47 -0500

    Update climbingStairs.java

--- climbingStairs.java
+++ climbingStairs.java
@@ -6,6 +6,9 @@ import java.util.*;
 class Main {
 
   public static int climbStairs(int n) {
+     if(n == 1) {
+            return 1;
+        }
     int[] dp = new int[n+1];
     dp[0] =1;
     dp[1] =1;
