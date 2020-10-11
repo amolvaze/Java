f62ff2f6014137cfe43e7e11a27be4ab6a028fd5
Amol Vaze <mailmeamol2112@gmail.com>
2020-10-11 12:11:35 -0500

    Update mergeKSortedList.java

--- mergeKSortedList.java
+++ mergeKSortedList.java
@@ -54,3 +54,39 @@ public ListNode mergeKLists(ListNode[] lists) {
         return prehead.next;
     }
 }
+
+// ---------------------------------------------------------------------Alternative solution--------------------------------------------------------------------------------------
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> q = new PriorityQueue(); // min heap.
+        for(ListNode head: lists){
+            while(head != null) {
+                q.add(head.val);
+                head = head.next;
+            }
+        }
+        
+        ListNode dummy = new ListNode(-1);
+        ListNode head = dummy;
+        while(!q.isEmpty()){
+            head.next = new ListNode(q.remove());
+            head = head.next;
+        }
+        
+        return dummy.next;
+    }
+}
+
+
+
