3fdafcfb1502eb8e293789244563e1ac4d2583e6
Amol Vaze <mailmeamol2112@gmail.com>
2020-10-11 17:09:33 -0500

    Update numbersDisappeared.java

--- numbersDisappeared.java
+++ numbersDisappeared.java
@@ -30,8 +30,33 @@ class Main {
         return result;
         
     }
+   
+    // Using extra space - with hashset
+
+  public static List<Integer> findDisappearedNumbers(int[] nums) { 
+    List<Integer> res = new ArrayList();
+    HashSet<Integer> set = new HashSet<>();
+    for(int num : nums){
+      set.add(num);
+    }
+   for(int i= 1; i<=nums.length; i++){
+       if(!set.contains(i)){
+         res.add(i);
+       }
+   }
+
+   return res;
+  }
+   
   public static void main(String[] args) {
     int[] arr = {4,3,2,7,8,2,3,1};
     System.out.println(findDisappearedNumbers(arr));
   }
 }
+
+
+
+
+
+
+
