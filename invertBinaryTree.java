7053e62354e63df5af0e2c5d1fb1f86c1a71ef03
Amol Vaze <mailmeamol2112@gmail.com>
2020-10-12 18:46:12 -0500

    Update invertBinaryTree.java

--- invertBinaryTree.java
+++ invertBinaryTree.java
@@ -36,3 +36,23 @@ class Solution {
         
     }
 }
+
+// Alternative Solution
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null)
+            return root;
+      TreeNode left = invertTree(root.left);
+      TreeNode right = invertTree(root.right);
+      root.left= right;
+      root.right= left;
+        return root;
+    }
+}
+
+
+
+
+
+
