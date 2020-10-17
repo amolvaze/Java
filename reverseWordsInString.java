12bf44f1c2e38f09e70d279f96fbf6bab4ad2b27
Amol Vaze <mailmeamol2112@gmail.com>
2020-10-16 19:17:43 -0500

    Update reverseWordsInString.java

--- reverseWordsInString.java
+++ reverseWordsInString.java
@@ -1,30 +1,19 @@
 import java.util.*;
-// code to reverse words in string
-
-import jdk.javadoc.internal.doclets.formats.html.resources.standard;
-
 class Main {
   public static String reverseWords(String s) {
-      if(s == null || s.length() == 0)
-      return "";
-      // spilt string by the space.
-      String[] arr = s.split(" ");
-     // for(int i =0; i < arr.length; i++)
-     // {
-      //  System.out.println("Arr contents: " +arr[i]);
-     // }
-      
-    StringBuilder sb = new StringBuilder();
-      // iterate through the string
-     for (int i = arr.length - 1; i >= 0; --i) {
-      {
-        if(!arr[i].equals(" "))
-        sb.append(arr[i]).append(" ");
-      }
- return sb.length() == 0 ? " " : sb.substring(0, sb.length()-1);
+   if(s.length() == 0 || s == null)
+    return "";
+   String[] str = s.toLowerCase().split(" ");
+   StringBuilder sb = new StringBuilder();
+   for(int i = str.length-1; i>=0; --i){
+    if(!str[i].equals(" ")){
+      sb.append(str[i]).append(" ");
+    } 
+   }
+return sb.length() == 0 ? " " : sb.substring(0, sb.length()-1);
   }
   public static void main(String[] args) {
-    String str = "the sky is blue";
-    System.out.println(reverseWords(str));
+    String s = "How are you";
+    System.out.println(reverseWords(s));
   }
 }
