f60e47aa51325290b4cb3c90630f2fa91b8ffec5
Amol Vaze <mailmeamol2112@gmail.com>
2020-11-02 15:35:18 -0600

    Create RemoveAllAdjacentDuplicatesInString.java

---
+++ RemoveAllAdjacentDuplicatesInString.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String removeDuplicates(String S) {
+        char[] stack = new char[S.length()];
+        int i =0;
+        for(int j=0; j< S.length(); j++) {
+            char current_char = S.charAt(j);
+            if( i > 0 && stack[i-1] == current_char){
+                i--;
+            }
+            else {
+             stack[i] = current_char;
+                i += 1;
+            }
+        }
+        
+         return new String(stack, 0, i);   
+    }
+}
