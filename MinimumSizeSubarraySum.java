9c399412a71c96b7801304715e2e178cd5a2bce4
Amol Vaze <mailmeamol2112@gmail.com>
2020-11-01 22:58:09 -0600

    Update MinimumSizeSubarraySum.java

--- MinimumSizeSubarraySum.java
+++ MinimumSizeSubarraySum.java
@@ -1,6 +1,10 @@
 
 // Time Complexity- O(n) and space complexity - O(1)
 
+/*Input: s = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: the subarray [4,3] has the minimal length under the problem constraint.*/
+
 class Main {
    public static int minSubArrayLen(int s, int[] nums) {
      int result = Integer.MAX_VALUE;
