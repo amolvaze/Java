cfb58ef9e8b2602af9356c61c64dca4c2fc73a7f
Amol Vaze <mailmeamol2112@gmail.com>
2020-11-04 16:02:40 -0600

    Update flipAndInvertImage.java

--- flipAndInvertImage.java
+++ flipAndInvertImage.java
@@ -1,3 +1,16 @@
+/*
+ Input: [[1,1,0],[1,0,1],[0,0,0]]
+Output: [[1,0,0],[0,1,0],[1,1,1]]
+Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+
+
+To flip an image horizontally means that each row of the image is reversed.  For example, flipping [1, 1, 0] horizontally results in [0, 1, 1].
+
+To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0. For example, inverting [0, 1, 1] results in [1, 0, 0].
+
+*/
+
 class Main {
    public static void flipAndInvertImage(int[][] A) {
     for(int i=0; i< A.length; i++){
