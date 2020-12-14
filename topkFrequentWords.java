168bb40ad83331137a0e515b079aca663e03d4b5
Amol Vaze <mailmeamol2112@gmail.com>
2020-12-14 12:29:31 -0600

    Update topkFrequentWords.java

--- topkFrequentWords.java
+++ topkFrequentWords.java
@@ -1,3 +1,27 @@
+// # Write a function top_k_words(phrase, k) that returns the K most common words in a given phrase.
+// # >>> example = 'once thrice twice thrice twice thrice'
+// # In the example phrase above, we have "once" once, "thrice" three times, and "twice" twice.
+ 
+// # If k=1, return the most common word, in this case "thrice".
+// # >>> top_k_words(phrase=example, k=1)
+// # ['thrice']
+  
+// # If k=2, return the two most common words in order of most common to least common.
+// # >>> top_k_words(phrase=example, k=2)
+// # ['thrice', 'twice']
+  
+// # If k=3, return the three most common words in order of most common to least common.
+// # >>> top_k_words(phrase=example, k=3)
+// # ['thrice', 'twice', 'once']
+  
+// # If k is greater than the number of different words, return all the words in order of most common to least common.
+// # >>> top_k_words(phrase=example, k=4)
+// # ['thrice', 'twice', 'once']
+ 
+// # What do you think is the right thing to do if k=0?
+
+// Time Complexity - O(n log k) 
+
 class Solution {
    public List<String> topKFrequent(String[] words, int k) {
         Map<String, Integer> count = new HashMap();
