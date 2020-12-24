5ba7e2f0e73850a006c4648f239420714aa12f95
Amol Vaze <mailmeamol2112@gmail.com>
2020-12-24 15:33:29 -0600

    Update wordsFormedByCharacters.java

--- wordsFormedByCharacters.java
+++ wordsFormedByCharacters.java
@@ -1,16 +1,16 @@
 
-Example 1:
+// Example 1:
 
-Input: words = ["cat","bt","hat","tree"], chars = "atach"
-Output: 6
-Explanation: 
-The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6.
-Example 2:
+// Input: words = ["cat","bt","hat","tree"], chars = "atach"
+// Output: 6
+// Explanation: 
+// The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6.
+// Example 2:
 
-Input: words = ["hello","world","leetcode"], chars = "welldonehoneyr"
-Output: 10
-Explanation: 
-The strings that can be formed are "hello" and "world" so the answer is 5 + 5 = 10.
+// Input: words = ["hello","world","leetcode"], chars = "welldonehoneyr"
+// Output: 10
+// Explanation: 
+// The strings that can be formed are "hello" and "world" so the answer is 5 + 5 = 10.
 
 import java.util.*;
 class Main{
