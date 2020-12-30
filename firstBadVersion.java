7199136a181d1aead07d6957fec034fabb5fb226
Amol Vaze <mailmeamol2112@gmail.com>
2020-12-30 15:39:30 -0600

    Update firstBadVersion.java

--- firstBadVersion.java
+++ firstBadVersion.java
@@ -1,3 +1,15 @@
+// Input: n = 5, bad = 4
+// Output: 4
+// Explanation:
+// call isBadVersion(3) -> false
+// call isBadVersion(5) -> true
+// call isBadVersion(4) -> true
+// Then 4 is the first bad version.
+
+// Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+// Using binary search approach - Time complexity - O(log n)
+
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int left =0;
