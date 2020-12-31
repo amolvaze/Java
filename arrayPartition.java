d36f42e2e77cb349f070efb2252af7de0b60ef28
Amol Vaze <mailmeamol2112@gmail.com>
2020-12-31 15:11:42 -0600

    Update arrayPartition.java

--- arrayPartition.java
+++ arrayPartition.java
@@ -10,6 +10,10 @@
 
 class Solution {
     public int arrayPairSum(int[] nums) {
+        
+        if(nums.length == 0 || nums == null)
+            return 0;
+        
         Arrays.sort(nums);
          int sum = 0;
          for(int i =0; i< nums.length-1; i+=2){
