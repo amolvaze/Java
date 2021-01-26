10b2c749653b33a80230870f83a15717637e8866
Amol Vaze <mailmeamol2112@gmail.com>
2021-01-25 22:28:12 -0600

    Update reverseLinkedList-||.java

--- reverseLinkedList-||.java
+++ reverseLinkedList-||.java
@@ -8,6 +8,11 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// Input: 1->2->3->4->5->NULL, m = 2, n = 4
+// Output: 1->4->3->2->5->NULL
+
+
 class Solution {
     public ListNode reverseBetween(ListNode head, int m, int n) {
         if(head == null)
