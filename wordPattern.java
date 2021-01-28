46ae6be2897e24dda59049432daf2a2b35b10272
Amol Vaze <mailmeamol2112@gmail.com>
2021-01-28 15:37:33 -0600

    Update wordPattern.java

--- wordPattern.java
+++ wordPattern.java
@@ -1,3 +1,28 @@
+// Given a pattern and a string s, find if s follows the same pattern.
+
+// Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+
+//   Example 1:
+
+// Input: pattern = "abba", s = "dog cat cat dog"
+// Output: true
+// Example 2:
+
+// Input: pattern = "abba", s = "dog cat cat fish"
+// Output: false
+// Example 3:
+
+// Input: pattern = "aaaa", s = "dog cat cat dog"
+// Output: false
+// Example 4:
+
+// Input: pattern = "abba", s = "dog dog dog dog"
+// Output: false
+
+// Time complexity : O(N)O(N) where NN represents the number of words in the s or the number of characters in the pattern.
+
+// Space complexity : O(M)O(M) where MM is the number of unique characters in pattern and words in s.
+
 import java.util.HashMap;
 
 class Main {
