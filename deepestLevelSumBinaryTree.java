336a13105ea469778fb3d45a67055f40890d181b
Amol Vaze <mailmeamol2112@gmail.com>
2021-02-02 15:21:41 -0600

    Update deepestLevelSumBinaryTree.java

--- deepestLevelSumBinaryTree.java
+++ deepestLevelSumBinaryTree.java
@@ -21,7 +21,7 @@ class Solution {
             int level_sum=0;
          
           while(!queue.isEmpty()){
-              level_sum =0;
+              level_sum =0; // we are resetting the sum when we visit each level.
            int size = queue.size();
            for(int i=0; i< size; i++){
             TreeNode current_node = queue.poll();
