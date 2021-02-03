e44bcb8e16197aa45d055f4a57e79442258f6d44
Amol Vaze <mailmeamol2112@gmail.com>
2021-02-03 15:52:08 -0600

    Create letterCombinationsPhoneNumbers.java

---
+++ letterCombinationsPhoneNumbers.java
@@ -0,0 +1,33 @@
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+Example 2:
+
+Input: digits = ""
+Output: []
+Example 3:
+
+Input: digits = "2"
+Output: ["a","b","c"]
+
+
+
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        LinkedList<String> output_arr = new LinkedList();
+        if(digits.length() == 0) return output_arr;
+        String[] char_map = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        
+        output_arr.add("");
+        for(int i=0; i< digits.length(); i++){
+         int index = Character.getNumericValue(digits.charAt(i));
+         while(output_arr.peek().length() == i){
+             String permutation = output_arr.remove();
+             for(char c : char_map[index].toCharArray()) {
+                output_arr.add(permutation + c); 
+             }
+         }
+        }
+        
+        return output_arr;
+    }
+}
