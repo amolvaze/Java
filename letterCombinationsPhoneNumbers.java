bd2be02ef1870735d9bf05981bb62475b065cc6a
Amol Vaze <mailmeamol2112@gmail.com>
2021-02-03 15:52:43 -0600

    Update letterCombinationsPhoneNumbers.java

--- letterCombinationsPhoneNumbers.java
+++ letterCombinationsPhoneNumbers.java
@@ -1,13 +1,15 @@
-Input: digits = "23"
-Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
-Example 2:
+//Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
 
-Input: digits = ""
-Output: []
-Example 3:
+// Input: digits = "23"
+// Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+// Example 2:
 
-Input: digits = "2"
-Output: ["a","b","c"]
+// Input: digits = ""
+// Output: []
+// Example 3:
+
+// Input: digits = "2"
+// Output: ["a","b","c"]
 
 
 
