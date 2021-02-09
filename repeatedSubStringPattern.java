637810dffb18b6d6c9c9953748dcc65291702e82
Amol Vaze <mailmeamol2112@gmail.com>
2021-02-09 16:14:52 -0600

    Update repeatedSubStringPattern.java

--- repeatedSubStringPattern.java
+++ repeatedSubStringPattern.java
@@ -1,3 +1,17 @@
+// Given a non-empty string check if it can be constructed by taking a substring of it and appending multiple copies of the substring together. 
+    
+// You may assume the given string consists of lowercase English letters only and its length will not exceed 10000.
+    
+// Example 1:
+
+// Input: "abab"
+// Output: True
+// Explanation: It's the substring "ab" twice.
+// Example 2:
+
+// Input: "aba"
+// Output: False
+    
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
         int len = s.length();
