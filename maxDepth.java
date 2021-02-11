cc0851fab3548a8b191230880a8844c905137ee7
Amol Vaze <mailmeamol2112@gmail.com>
2021-02-11 15:43:17 -0600

    Update maxDepth.java

--- maxDepth.java
+++ maxDepth.java
@@ -29,3 +29,50 @@ class Solution {
         
     }
 }
+
+// Using BFS - Queue Approach
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+  public int maxDepth(Node root) {
+    
+        if(root == null) return 0;
+        
+         int depth=0;
+         
+         Queue<Node> queue = new LinkedList();
+            queue.offer(root);
+            
+            while(!queue.isEmpty()){
+              int size = queue.size();
+                for(int i=0; i< size; i++){
+                Node current_node = queue.poll();
+                 for(Node child: current_node.children) {
+                     queue.offer(child);
+                 }
+                }
+                
+                depth++;
+            }
+        
+         return depth;
+        
+        
+    }
