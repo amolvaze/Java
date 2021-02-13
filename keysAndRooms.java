ec832e077dd3817d8d7cdc33531c917984c9bc2b
Amol Vaze <mailmeamol2112@gmail.com>
2021-02-13 14:57:12 -0600

    Update keysAndRooms.java

--- keysAndRooms.java
+++ keysAndRooms.java
@@ -1,3 +1,17 @@
+// Example 1:
+
+// Input: [[1],[2],[3],[]]
+// Output: true
+// Explanation:  
+// We start in room 0, and pick up key 1.
+// We then go to room 1, and pick up key 2.
+// We then go to room 2, and pick up key 3.
+// We then go to room 3.  Since we were able to go to every room, we return true.
+    
+// Time Complexity: O(N + E)O(N+E), where NN is the number of rooms, and EE is the total number of keys.
+
+//Space Complexity: O(N)O(N) in additional space complexity, to store stack and seen.
+
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         boolean[] seen = new boolean[rooms.size()];
