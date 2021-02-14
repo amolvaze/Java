12c0ab267d7c6410596382b2bae14b49e544f7cb
Amol Vaze <mailmeamol2112@gmail.com>
2021-02-13 19:07:38 -0600

    Update findBottomLeftTreeValue.java

--- findBottomLeftTreeValue.java
+++ findBottomLeftTreeValue.java
@@ -20,6 +20,9 @@ import java.util.*;
 class Main {
  
   public static int findBottomLeftValue(TreeNode root) {
+   if(root.left==null && root.right==null){
+            return root.val;
+        }
    Queue<TreeNode> queue = new LinkedList<>();
    queue.offer(root);
    while(!queue.isEmpty())
@@ -43,3 +46,30 @@ class Main {
     System.out.println("Hello world!");
   }
 }
+
+// Alternaive solution using Queue - level order traversal
+
+ public int findBottomLeftValue(TreeNode root) {
+        if(root.left==null && root.right==null){
+            return root.val;
+        }
+      Queue<TreeNode> queue=new LinkedList<>();
+        int leftminimum=0;
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            for(int i=0;i<size;i++){
+                TreeNode current=queue.remove();
+                if(i==0){
+                    leftminimum=current.val;
+                }
+                if(current.left!=null){
+                    queue.add(current.left);
+                }
+                if(current.right!=null){
+                    queue.add(current.right);
+                }
+            }
+        }
+        return leftminimum;
+    }
