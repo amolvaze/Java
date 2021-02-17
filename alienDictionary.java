da8a5db5f0549bb4a84cd01acee671054572b376
Amol Vaze <mailmeamol2112@gmail.com>
2021-02-17 15:28:03 -0600

    Update alienDictionary.java

--- alienDictionary.java
+++ alienDictionary.java
@@ -39,3 +39,51 @@ class Solution {
         }
     }
 }
+
+// -----------------------------------JavaScript solution-------------------------------------
+
+/**
+ * @param {string[]} words
+ * @param {string} order
+ * @return {boolean}
+ Time Complexity : O(n) Traversing Through the entire list of words
+Space Complexity : O(1) Use of a constant space hashmap
+ */
+const isAlienSorted = (words, order) => {
+  const map = {};
+
+  for (let i = 0; i < order.length; i += 1) {
+    map[order[i]] = i;
+  }
+
+  outer:
+  for (let i = 0; i < words.length - 1; i += 1) {
+    const current = words[i];
+    const next = words[i + 1];
+    const minLength = Math.min(current.length, next.length);
+
+    for (let i = 0; i < minLength; i += 1) {
+      if (current[i] === next[i]) continue;
+      if (map[current[i]] < map[next[i]]) continue outer;
+      if (map[current[i]] > map[next[i]]) return false;
+    }
+
+    if (current.length > next.length) return false;
+  }
+
+  return true;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
