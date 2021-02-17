7704832271550eb9fd9eed2e8fe5ceabe1a481e9
Amol Vaze <mailmeamol2112@gmail.com>
2021-02-17 14:45:46 -0600

    Update removeElement.java

--- removeElement.java
+++ removeElement.java
@@ -1,3 +1,10 @@
+// Remove element from an array in place - without modifying.
+
+// Time complexity : O(n)O(n). Both ii and nn traverse at most nn steps. 
+//In this approach, the number of assignment operations is equal to the number of elements to remove. So it is more efficient if elements to remove are rare.
+
+// Space Complexity - O(1) - constance space as no extra space and we are doing in place.
+
 class Solution {
     public int removeElement(int[] nums, int val) {
     int i = 0;
