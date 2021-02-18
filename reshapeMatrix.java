ea27f9477ef2d8a1db2356019d6e137f26c69f8e
Amol Vaze <mailmeamol2112@gmail.com>
2021-02-18 14:51:07 -0600

    Create reshapeMatrix.java

---
+++ reshapeMatrix.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        
+        int rows = nums.length;
+        int cols = nums[0].length;
+        
+        if((rows * cols) != (r*c)) return nums;
+        
+        int[][] output_arr = new int[r][c];
+        int row_num=0;
+        int col_num=0;
+        
+        for(int i=0; i< rows; i++){
+            for(int j=0; j< cols; j++){
+                output_arr[row_num][col_num] = nums[i][j];
+                col_num++;
+                if(col_num == c){
+                    col_num=0;
+                    row_num++;
+                }
+            }
+        }
+        
+        return output_arr;
+    }
+}
