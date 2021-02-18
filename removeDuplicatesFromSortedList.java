d2da3d9f8ccc2a384c94929a20624f5e841b66f5
Amol Vaze <mailmeamol2112@gmail.com>
2021-02-18 17:31:33 -0600

    Update removeDuplicatesFromSortedList.java

--- removeDuplicatesFromSortedList.java
+++ removeDuplicatesFromSortedList.java
@@ -8,6 +8,13 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// Time complexity : O(n)O(n). Because each node in the list is checked exactly once to determine 
+
+// if it is a duplicate or not, the total run time is O(n)O(n), where nn is the number of nodes in the list.
+
+// Space complexity : O(1)O(1). No additional space is used.
+
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         ListNode current_node = head;
