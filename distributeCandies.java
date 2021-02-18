107c6ebdcd9a16d313a9e14c98f510ebca6927a1
Amol Vaze <mailmeamol2112@gmail.com>
2021-02-18 15:45:36 -0600

    Update distributeCandies.java

--- distributeCandies.java
+++ distributeCandies.java
@@ -1,3 +1,13 @@
+// Example 1:
+
+// Input: candies = 7, num_people = 4
+// Output: [1,2,3,1]
+// Explanation:
+// On the first turn, ans[0] += 1, and the array is [1,0,0,0].
+// On the second turn, ans[1] += 2, and the array is [1,2,0,0].
+// On the third turn, ans[2] += 3, and the array is [1,2,3,0].
+// On the fourth turn, ans[3] += 1 (because there is only one candy left), and the final array is [1,2,3,1].
+
 class Solution {
     public int[] distributeCandies(int candies, int num_people) {
         int[] result = new int[num_people];
