7647d1466efeae2cd20db12311c8adaeb19fd1fa
Amol Vaze <mailmeamol2112@gmail.com>
2021-02-19 10:58:43 -0600

    Update monotonicArray.java

--- monotonicArray.java
+++ monotonicArray.java
@@ -1,3 +1,22 @@
+// An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+// An array A is monotone increasing if for all i <= j, A[i] <= A[j].  An array A is monotone decreasing if for all i <= j, A[i] >= A[j].
+
+// Return true if and only if the given array A is monotonic.
+    
+// Example 1:
+
+// Input: [6,5,4,4]
+// Output: true
+
+// Example 2:
+// Input: [1,3,2]
+// Output: false
+
+// Time Complexity - O(n)
+
+//Space Complexity - O(1)
+
 class Solution {
     public boolean isMonotonic(int[] A) {
         
