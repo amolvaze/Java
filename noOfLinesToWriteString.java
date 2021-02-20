b9211188e2d247ff94781e7156c826e0a1943986
Amol Vaze <mailmeamol2112@gmail.com>
2021-02-20 15:33:12 -0600

    Update noOfLinesToWriteString.java

--- noOfLinesToWriteString.java
+++ noOfLinesToWriteString.java
@@ -1,3 +1,16 @@
+// Input: widths = [10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10], s = "abcdefghijklmnopqrstuvwxyz"
+// Output: [3,60]
+// Explanation: You can write s as follows:
+// abcdefghij  // 100 pixels wide
+// klmnopqrst  // 100 pixels wide
+// uvwxyz      // 60 pixels wide
+// There are a total of 3 lines, and the last line is 60 pixels wide.
+
+// Time Complexity: O(S\text{.length})O(S.length), as we iterate through S.
+
+// Space Complexity - O(1) additional space, as we only use lines and width. 
+//In Java, our toCharArray method makes this O(S\text{.length})O(S.length), but we could use .charAt instead).
+
 class Solution {
     public int[] numberOfLines(int[] widths, String s) {
         int[] result = new int[2];
