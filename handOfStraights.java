890fc51feb3de2f3171db0f2d749f470df7abe9c
Amol Vaze <mailmeamol2112@gmail.com>
2021-02-20 14:42:36 -0600

    Update handOfStraights.java

--- handOfStraights.java
+++ handOfStraights.java
@@ -21,35 +21,32 @@
 
 class Solution {
     public boolean isNStraightHand(int[] hand, int W) {
+     
+        if(hand.length % W != 0) return false;
         
-        if(hand.length % W !=0) return false;
+        TreeMap<Integer, Integer> card_counts = new TreeMap<>();
         
-        TreeMap<Integer, Integer> card_counts = new TreeMap();
-         
         for(int card : hand){
-          
             if(!card_counts.containsKey(card)){
-               
                 card_counts.put(card, 1);
-
-            } else {
+            }
+            else {
                 card_counts.replace(card, card_counts.get(card) + 1);
             }
-            
         }
         
         while(card_counts.size() > 0){
             int min_val = card_counts.firstKey();
             
-            for(int card = min_val; card < min_val + W; card++) {
+            for(int card=min_val; card < min_val + W; card++ ){
+                
                 if(!card_counts.containsKey(card)) return false;
-                int count = card_counts.get(card);
-                if(count == 1) card_counts.remove(card);
-                else card_counts.replace(card, count - 1);   
+                    int count = card_counts.get(card);
+                    if(count == 1) card_counts.remove(card);
+                    else card_counts.replace(card, card_counts.get(card) - 1);
+            }
         }
         
-        
-    }
         return true;
-}
+    }
 }
