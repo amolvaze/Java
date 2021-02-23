895a2a68f3ef085f15e8b206011b0c24d9cf7250
Amol Vaze <mailmeamol2112@gmail.com>
2021-02-23 10:26:39 -0600

    Update floodFill.java

--- floodFill.java
+++ floodFill.java
@@ -1,3 +1,18 @@
+// Input: 
+// image = [[1,1,1],[1,1,0],[1,0,1]]
+// sr = 1, sc = 1, newColor = 2
+// Output: [[2,2,2],[2,2,0],[2,0,1]]
+// Explanation: 
+// From the center of the image (with position (sr, sc) = (1, 1)), all pixels connected 
+// by a path of the same color as the starting pixel are colored with the new color.
+// Note the bottom corner is not colored 2, because it is not 4-directionally connected
+// to the starting pixel.
+
+//DFS approach
+
+// Time Complexity - O(N)
+// Space Complexity - O(N)
+
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
         if(image[sr][sc] == newColor) return image;
