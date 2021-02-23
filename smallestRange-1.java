b9de4108f8a49dae0b4c015420be4218ae683ef4
Amol Vaze <mailmeamol2112@gmail.com>
2021-02-23 14:54:15 -0600

    Update smallestRange-1.java

--- smallestRange-1.java
+++ smallestRange-1.java
@@ -1,3 +1,21 @@
+// Given an array A of integers, for each integer A[i] we may choose any x with -K <= x <= K, and add x to A[i].
+
+// After this process, we have some array B.
+
+// Return the smallest possible difference between the maximum value of B and the minimum value of B.
+
+// Example 1:
+
+// Input: A = [1], K = 0
+// Output: 0
+// Explanation: B = [1]
+// Example 2:
+
+// Input: A = [0,10], K = 2
+// Output: 6
+// Explanation: B = [2,8]
+
+
 class Solution {
     public int smallestRangeI(int[] A, int K) {
         int min_val = A[0];
