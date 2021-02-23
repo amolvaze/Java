3cf97b75779d586bc703666c4952d28ba599aa21
Amol Vaze <mailmeamol2112@gmail.com>
2021-02-23 10:37:39 -0600

    Update palindromePermutation.java

--- palindromePermutation.java
+++ palindromePermutation.java
@@ -1,5 +1,25 @@
+// Given a string, determine if a permutation of the string could form a palindrome.
+
+// Example 1:
+
+// Input: "code"
+// Output: false
+
+// Example 2:
+
+// Input: "aab"
+// Output: true
+    
 public class Solution {
     public boolean canPermutePalindrome(String s) {
-       
-    }
+       int[] char_counts = new int[128];
+       for(int i=0 ;i< s.length(); i++ ){
+        char_counts[s.charAt(i)]++;
+       }
+        
+        int count = 0;
+        for(int i =0; i < 128; i++){
+         count += char_counts[i]%2;        }
+        return count <= 1;
+     }
 }
