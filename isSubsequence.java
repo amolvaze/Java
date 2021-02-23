743c97324dd2de87edd4024c899c4df46ef4080e
Amol Vaze <mailmeamol2112@gmail.com>
2021-02-23 10:59:51 -0600

    Update isSubsequence.java

--- isSubsequence.java
+++ isSubsequence.java
@@ -1,3 +1,19 @@
+// Given two strings s and t, check if s is a subsequence of t.
+
+// A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) 
+// of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+//  Example 1:
+
+// Input: s = "abc", t = "ahbgdc"
+// Output: true
+// Example 2:
+
+// Input: s = "axc", t = "ahbgdc"
+// Output: false
+
+// Time complexity - O(N) and Space Complexity - O(1) - constant space. 
+
 class Solution {
     public boolean isSubsequence(String s, String t) {
         
