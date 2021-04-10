cba19092310d8a05812fc5c43f725d80b180acfc
Amol Vaze <mailmeamol2112@gmail.com>
2021-04-10 15:42:19 -0400

    Create LemonadeChange.java

---
+++ LemonadeChange.java
@@ -0,0 +1,29 @@
+// Time Complexity - O(N) - where N is the length of the bills.
+
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+      
+        int fives =0;
+        int tens = 0;
+         
+        for(Integer bill: bills){
+            if(bill == 5){
+                fives++;
+            }else if(bill == 10){
+                tens++;
+                fives--;
+            }
+            else if(tens > 0){
+                tens--;
+                fives--;
+            }
+            else{
+                fives-=3;
+            }
+        
+        if(fives < 0) return false;
+        }
+        
+        return true;
+    }
+}
