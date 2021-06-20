cf36ca792aa05fd12140f36469863e7e8c345649
Amol Vaze <mailmeamol2112@gmail.com>
2021-06-19 23:34:22 -0400

    Update findtheWinnerOfCircularGame.java

--- findtheWinnerOfCircularGame.java
+++ findtheWinnerOfCircularGame.java
@@ -7,7 +7,24 @@ Count the next k friends in the clockwise direction including the friend you sta
 The last friend you counted leaves the circle and loses the game.
 If there is still more than one friend in the circle, go back to step 2 starting from the friend immediately clockwise of the friend who just lost and repeat.
 Else, the last friend in the circle wins the game.
-Given the number of friends, n, and an integer k, return the winner of the game. */
+Given the number of friends, n, and an integer k, return the winner of the game. 
+
+
+Input: n = 5, k = 2
+Output: 3
+Explanation: Here are the steps of the game:
+1) Start at friend 1.
+2) Count 2 friends clockwise, which are friends 1 and 2.
+3) Friend 2 leaves the circle. Next start is friend 3.
+4) Count 2 friends clockwise, which are friends 3 and 4.
+5) Friend 4 leaves the circle. Next start is friend 5.
+6) Count 2 friends clockwise, which are friends 5 and 1.
+7) Friend 1 leaves the circle. Next start is friend 3.
+8) Count 2 friends clockwise, which are friends 3 and 5.
+9) Friend 5 leaves the circle. Only friend 3 is left, so they are the winner.
+
+
+*/
 
 // Using Queue Data  Structure - O(n.k)
 
@@ -24,4 +41,9 @@ class Solution {
         }
         return Q.peek();
     }
+    
+    public static void main(String[] args) {
+    int n = 5, k= 2;
+    System.out.println(findTheWinner(n,k)); // output = 3 
+  }
 }
