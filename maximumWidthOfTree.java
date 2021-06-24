e0b8a321c4614201d0454c22ea4b8e4bcb936554
Amol Vaze <mailmeamol2112@gmail.com>
2021-06-23 21:55:52 -0400

    Update maximumWidthOfTree.java

--- maximumWidthOfTree.java
+++ maximumWidthOfTree.java
@@ -15,6 +15,41 @@
  *     }
  * }
  */
+
+// Iterative Solution using Queue
+class Solution {
+public int widthOfBinaryTree(TreeNode root) {
+        if (root == null)
+            return 0;
+        int width = 1;
+        Queue<TreeNode> queue = new LinkedList<>();
+        LinkedList<Integer> list = new LinkedList<>();
+        queue.offer(root);
+        list.add(1);
+        while (!queue.isEmpty()) {
+            int cnt = queue.size();
+            for (int i = 0; i < cnt; ++i) {
+                TreeNode node = queue.poll();
+                int index = list.removeFirst();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                    list.add(2 * index);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                    list.add(2 * index + 1);
+                }
+            }
+            if (list.size() > 1)
+                width = Math.max(width, list.getLast() - list.getFirst() + 1);
+        }
+        return width;
+    }
+
+}
+
+//----------------------------------------Recursive Solution--------------------------------
+
 class Solution {
     int maxWidth;
     HashMap<Integer,Integer> leftmost_positions;
@@ -34,7 +69,7 @@ class Solution {
     }
 }
 
-// Alternative solution using queue - level order
+// -------------------Alternative solution using queue using level order--------------------------------
 
 public int widthOfBinaryTree(TreeNode root) {
         if(root == null) return 0;
